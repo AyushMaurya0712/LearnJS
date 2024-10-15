f36de243bef3be79be0c66aa1a106764a0160a17
AyushMaurya0712 <163465651+AyushMaurya0712@users.noreply.github.com>
2024-10-15 13:52:07 +0000

    data types

---
+++ 4_Variables.js
@@ -0,0 +1,19 @@
+const userId = 12345 ; 
+let email = "ayush@email.com" ; 
+var name = "ayush" ; 
+car = "lambo"  ; 
+
+// const varaibles once should cant be changed 
+
+/*
+var is not used because of the issue in block scope and 
+functional scope 
+*/
+
+console.log(userId) ; 
+
+let alpha ; 
+
+console.table([userId , email , name , car , alpha]) ; 
+
+// the variables who values is not set they are stated as undefined in js 
---
+++ 5_DataTypes.js
@@ -0,0 +1,21 @@
+"use strict"  // treats all js code as newer version of code 
+
+// alert("aware")  - we are using js not the browser 
+
+// write the code that should be easily redable 
+
+console.log("ratan") ; 
+
+/*  types of data 
+number 
+bigInt 
+boolean 
+double 
+string 
+symbol 
+null 
+undefined 
+*/ 
+
+console.log(typeof null) ;        // it is an     OBJECT 
+console.log(typeof undefined) ;   // it is an     undefined
